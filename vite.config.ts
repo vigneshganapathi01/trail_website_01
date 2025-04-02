8819cf07eb0eae1e6a0105dd63649520ec52e711
vigneshganapathi01 <vigneshg2k01@gmail.com>
2025-04-02 15:13:53 +0530

    main

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/trail_website_01/", // Ensure this matches your repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
